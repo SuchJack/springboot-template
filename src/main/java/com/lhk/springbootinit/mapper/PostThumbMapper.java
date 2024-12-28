d85e52776bc567228a8820c8c1877f9875d67709
SuchJack <2640808535@qq.com>
2024-12-28 19:20:50 +0800

    first commit

---
+++ src/main/java/com/lhk/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.lhk.springbootinit.mapper;
+
+import com.lhk.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/lhk/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lhk.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lhk.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
