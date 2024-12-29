559c4a6f6504f14599e441e0301b0298858b071c
SuchJack <2640808535@qq.com>
2024-12-29 20:53:17 +0800

    update

--- src/main/java/com/lhk/springbootinit/constant/FileConstant.java
+++ src/main/java/com/lhk/springbootinit/constant/FileConstant.java
@@ -9,5 +9,6 @@ public interface FileConstant {
      * COS 访问地址
      * todo 需替换配置
      */
-    String COS_HOST = "https://such-jack.top";
+    String COS_HOST = "https://lhk-generator-1256961743.cos.ap-guangzhou.myqcloud.com";
+
 }
